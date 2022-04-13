39485965d7f33275acd45629759d5f585468f30b
Andrew Rodriguez <andrewrb22@gmail.com>
2022-04-13 19:20:31 -0400

    Question #1 COMPLETED

--- HW7.C
+++ HW7.C
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #define SIZE 5
 
+
+// 1) What does this code print to the screen
+void print(int *);
+void print(int *a)
+{
+    int index = 0 ;
+    while (index < SIZE)
+    {
+        printf("array[%d] = %d\n", index, a[index++]);
+    }
+}
+//----------------------------------------------//
 int main()
 {
-    printf("Working on HW 7");
-    return 0;
+    int array[5] = {41, 5, 8, 9, 5};
+    print(array);
+     return 0;
 }
+
+//-------RESULT---------//
+//array[1] = 41
+//array[2] = 5
+//array[3] = 8
+//array[4] = 9
+//array[5] = 5

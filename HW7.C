db5c0aff6171d1adf276934961853f48175418a8
Andrew Rodriguez <andrewrb22@gmail.com>
2022-04-13 20:22:33 -0400

    Question # 2 completed

--- HW7.C
+++ HW7.C
@@ -2,28 +2,42 @@
 #include <stdlib.h>
 #define SIZE 5
 
-
-// 1) What does this code print to the screen
+//-----------------PRINT FUNCTION---------------------------
 void print(int *);
 void print(int *a)
 {
-    int index = 0 ;
+    int index = 0;
     while (index < SIZE)
     {
         printf("array[%d] = %d\n", index, a[index++]);
     }
 }
-//----------------------------------------------//
+
+//------------SWAP FUNCTION----------------------------------//
+void swap(int *p1, int *p2);
+void swap(int *p1, int *p2)
+{
+    int temp ;
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+    
+       
+    
+}
+//------------------MAIN FUNCTION----------------------------//
 int main()
 {
+    int i ;
     int array[5] = {41, 5, 8, 9, 5};
+    swap(&array[0], &array[1]);
     print(array);
-     return 0;
+    return 0;
 }
 
 //-------RESULT---------//
-//array[1] = 41
-//array[2] = 5
-//array[3] = 8
-//array[4] = 9
-//array[5] = 5
+// array[1] = 5
+// array[2] = 41
+// array[3] = 8
+// array[4] = 9
+// array[5] = 5

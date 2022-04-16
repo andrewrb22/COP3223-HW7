12d2a90ed8a317555fa98bb0d56e7d77e52df976
Andrew Rodriguez <andrewrb22@gmail.com>
2022-04-15 21:01:21 -0400

    testing more, cleaning

--- HW7.C
+++ HW7.C
@@ -17,13 +17,10 @@ void print(int *a)
 void swap(int *p1, int *p2);
 void swap(int *p1, int *p2)
 {
-    int temp ;
+    int temp;
     temp = *p1;
     *p1 = *p2;
     *p2 = temp;
-    
-       
-    
 }
 
 //----------------SWAP_ELEMENTS FUNCTION-------------------------//
@@ -31,42 +28,25 @@ void swap(int *p1, int *p2)
 int swap_elements(int *a)
 {
     int i, swapped;
-    for( i = 0 ;  i < SIZE -1; i++)
+    for (i = 0; i < SIZE - 1; i++)
     {
-        for(swapped = 0; swapped < SIZE - 1; swapped++){
-            if (a[swapped]> a[swapped + 1])
-            swap(&a[swapped], &a[swapped + 1]);
+        for (swapped = 0; swapped < SIZE - 1; swapped++)
+        {
+            if (a[swapped] > a[swapped + 1])
+                swap(&a[swapped], &a[swapped + 1]);
         }
     }
-   
-
-    
 }
 //------------------MAIN FUNCTION----------------------------//
+
 int main()
 {
     int keep_swapping = 0;
-    int array [5]= {15, 11, 88, 154, 44};
-    // int pass = 1;
-    
-    // keep_swapping = swap_elements(array); 
-    swap_elements(array);
+    int array[5] = {15, 11, 88, 154, 44};
 
-    // while ( keep_swapping == 1)
-    // {
-    //     keep_swapping = swap_elements(array);
-    //     printf("PASS NUMBER %d\n", pass++);
-    //     print(array);
-    //     printf("++++++++++++++++++++++++++\n");
-    // }
-    
-    printf ("\n\n\nTHE BUBBLE SORT ALGORITHM\n\n\n"); 
+    swap_elements(array);
+    printf("\n\n\nTHE BUBBLE SORT ALGORITHM\n\n\n");
     print(array);
-    
 
     return 0;
 }
-
-//-------RESULT---------//
-
-// THE BUBBLE SORT ALGORITHM
